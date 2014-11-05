5ca395b56e73749cebd0a1fe351f438f5d832885
Blake Elias <blakeelias@gmail.com>
2014-11-05 15:22:47 -0500

    Add unit test and some debug print statements

--- homework2/svm.py
+++ homework2/svm.py
@@ -3,6 +3,8 @@ from scipy import optimize
 import pylab as pl
 #from mpl_toolkits.mplot3d.axes3d import Axes3D
 
+import unittest
+
 def svm(x, y, slackTightness):
     '''Computes a support vector machine from training data 'data' (which is in the form:
     data = [(x, classification), ...]
@@ -61,6 +63,8 @@ def numericalGradient(func, intervalWidth = 1e-3):
     return gradient
 
 def svmMultipliers(x, y, slackTightness):
+    print('x', x)
+    print('len(x)', len(x))
     def objectiveFunction(multipliers):
         return sum(multipliers) - 0.5 * sum([
             sum([
@@ -81,19 +85,56 @@ def svmMultipliers(x, y, slackTightness):
                         sum([mult[i] * y[i]
                             for i in range(len(mult))])})
 
+    x0 = np.random.randn(len(x[0]))
+    res_cons = optimize.minimize(objectiveFunction,x0,jac=jacobian,constraints=constraints,
+        method='SLSQP',options={'disp':False})
+
+    return res_cons['x']
+
+def testOptimize():
+    def objectiveFunction(args):
+        x, y = args
+        return x*x + y*y
+
+    jacobian = numericalGradient(objectiveFunction)
+
+    constraints = ({'type': 'ineq',
+                    'fun': lambda mult: mult},
+
+                    {'type': 'ineq',
+                    'fun': lambda mult: slackTightness - mult},
+
+                    {'type': 'eq',
+                    'fun': lambda mult: 
+                        sum([mult[i] * y[i]
+                            for i in range(len(mult))])})
+
     x0 = np.random.randn(len(x))
     res_cons = optimize.minimize(objectiveFunction,x0,jac=jacobian,constraints=constraints,
         method='SLSQP',options={'disp':False})
 
     return res_cons['x']
 
+class TestSVM(unittest.TestCase):
+
+    def test_svmWeights(self):
+        x = map(lambda xx: np.array(xx), [(1, 2), (2, 2), (0, 0), (-2, 3)])
+        y = [1, 1, -1, -1]
+        weights = [2, 3, 4, 5]
+        print(svmWeights(x, y, weights))
+        print(np.array((18, -5)))
+
+    def test_svmMultipliers(self):
+        pass
 
 def main():
     x = [(1, 2), (2, 2), (0, 0), (-2, 3)]
     y = [1, 1, -1, -1]
-    classifier = svm(map(lambda xx: np.array(xx), x), y, 100)
+    classifier = svm(map(lambda xx: np.array(xx), x), y, 10)
     print(classifier(np.array((10, 10))))
     print(classifier(np.array((-10, -10))))
 
 if __name__ == '__main__':
+    #testOptimize()
     main()
+    #unittest.main()
