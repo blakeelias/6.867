18baeb02a5deffc897445efaf6add3d76e812eea
Blake Elias <blakeelias@gmail.com>
2014-11-06 15:53:09 -0500

    Add SVM code using cvxopt's svmcmpl

--- homework2/svm_test.py
+++ homework2/svm_test.py
@@ -1,31 +1,57 @@
 from numpy import *
 from plotBoundary import *
-import svm
+#import svm
+from cvxopt import matrix
+import svmcmpl
 
 # parameters
-name = 'ls'
+#name = 'ls'
+name = 'small'
 print '======Training======'
 # load data from csv files
 train = loadtxt('data/data_'+name+'_train.csv')
 # use deep copy here to make cvxopt happy
 X = train[:, 0:2].copy()
+#X = [hstack((x, array(1,))) for x in X]
 Y = train[:, 2:3].copy()
 
+print('X')
+print(X)
+print('Y')
+print(Y)
 # Carry out training, primal and/or dual
 ### TODO ###
-predictSVM = svm.svm(X, Y, 1)
+#predictSVM = svm.svm(X, Y, 1)
 # Define the predictSVM(x) function, which uses trained parameters
 ### TODO ###
 
+#sol = svmcmpl.softmargin(matrix([[1., 2.], [2., 2.], [0., 0.], [-2., 3.]]).T, matrix([1., 1., -1., -1.]), 1)
+sol = svmcmpl.softmargin(matrix(X), matrix(Y), 1)
+#print(sol)
+
+predictSVM = sol['classifier']
+
+#print(predictSVM(matrix([[2., 2.], [2., 2.], [2., 2.], [3., 2.], [-2., 2.]]).T))
+
 # plot training results
-plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
+#plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
+print(predictSVM(matrix(X)))
 
 
 print '======Validation======'
 # load data from csv files
-validate = loadtxt('data/data_'+name+'_validate.csv')
+#validate = loadtxt('data/data_'+name+'_validate.csv')
+validate = loadtxt('data/data_'+name+'_test.csv')
 X = validate[:, 0:2]
 Y = validate[:, 2:3]
 # plot validation results
-plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Validate')
+#plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Validate')
 
+yPredicted = predictSVM(matrix(X))
+nError = 0
+for i in range(len(yPredicted)):
+    if yPredicted[i] != Y[i]:
+        nError += 1
+
+print('nError', nError)
+print('nTotal', len(yPredicted))
