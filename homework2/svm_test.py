869466f85b177a0f945e79a7326f2c11a892c540
Blake Elias <blakeelias@gmail.com>
2014-11-06 19:42:24 -0500

    Show data from all datasets

--- homework2/svm_test.py
+++ homework2/svm_test.py
@@ -8,7 +8,7 @@ from svm import svmWeights
 
 # parameters
 #name = 'ls'
-print('name', 'error on training set', 'error on validation set')
+print('name', 'C', 'b', 'error on validation set') # 'error on training set',
 for name in ['smallOverlap', 'bigOverlap', 'ls', 'nonsep']:
 
     #print '======Training======'
@@ -30,46 +30,47 @@ for name in ['smallOverlap', 'bigOverlap', 'ls', 'nonsep']:
     ### TODO ###
 
     #print('C', 'b', 'error rate')
-    #for C in [0.01, 0.1, 1, 10, 100]:
-    #    for b in np.arange(1, 10, 1):
+    for C in [0.01, 0.1, 1, 10, 100]:
+        for b in np.arange(1, 10, 1):
             # b = 1/(2*sigma)
             # sigma = 1/(2*b)
 
-    C = 1
-    sol = svmcmpl.softmargin(matrix(X), matrix(Y), C, kernel='linear') #kernel='rbf', sigma=1.0/(2*b))
-    predictSVM = sol['classifier']
-    errors = sol['misclassified']
-    totalError = len(errors[0] + errors[1])
-    trainingErrorRate = totalError*1.0/len(X)
+            #C = 1
+            sol = svmcmpl.softmargin(matrix(X), matrix(Y), C, kernel='rbf', sigma=1.0/(2*b))
+            predictSVM = sol['classifier']
+            errors = sol['misclassified']
+            totalError = len(errors[0] + errors[1])
+            trainingErrorRate = totalError*1.0/len(X)
 
-    # plot training results
-    #plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
-    #print(predictSVM(matrix(X)))
+            # plot training results
+            #plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
+            #print(predictSVM(matrix(X)))
 
-    #print '======Validation======'
-    # load data from csv files
-    #validate = loadtxt('data/data_'+name+'_validate.csv')
-    validate = loadtxt('data/data_'+name+'_test.csv')
-    X = validate[:, 0:2]
-    Y = validate[:, 2:3]
-    # plot validation results
-    #plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Validate')
+            #print '======Validation======'
+            # load data from csv files
+            #validate = loadtxt('data/data_'+name+'_validate.csv')
+            validate = loadtxt('data/data_'+name+'_test.csv')
+            X = validate[:, 0:2].copy()
+            Y = validate[:, 2:3].copy()
+            # plot validation results
+            #plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Validate')
 
-    yPredicted = predictSVM(matrix(X))
-    nError = 0
-    for i in range(len(yPredicted)):
-        if yPredicted[i] != Y[i]:
-            nError += 1
-    validationErrorRate = nError*1.0/len(yPredicted)
+            yPredicted = predictSVM(matrix(X))
+            nError = 0
+            for i in range(len(yPredicted)):
+                if yPredicted[i] != Y[i]:
+                    nError += 1
+            validationErrorRate = nError*1.0/len(yPredicted)
 
-    #print('weights')
-    #print(weights)
-    #print('geometric margin 1/||w||')
+            #print('weights')
+            #print(weights)
+            #print('geometric margin 1/||w||')
 
-    print(name, trainingErrorRate, validationErrorRate)
+            print(name, C, b, validationErrorRate) # trainingErrorRate
+            print('')
 
-    #print('nError', nError)
-    #print('nTotal', len(yPredicted))
+            #print('nError', nError)
+            #print('nTotal', len(yPredicted))
 
 
 
